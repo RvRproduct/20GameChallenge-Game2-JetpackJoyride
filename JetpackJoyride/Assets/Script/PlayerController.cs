f213cf5ec1ef4e66e2363b0defae9584a633dabf
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2025-08-09 17:48:42 -0600

    We have a game kinda

--- JetpackJoyride/Assets/Script/PlayerController.cs
+++ JetpackJoyride/Assets/Script/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -52,5 +53,19 @@ public class PlayerController : MonoBehaviour
         isUsingJetpack = false;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<IObstacle>() != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<IObstacle>() != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
 }
