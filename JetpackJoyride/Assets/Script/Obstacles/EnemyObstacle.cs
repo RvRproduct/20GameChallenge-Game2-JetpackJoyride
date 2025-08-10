355039c659d2c3b023d644a6e9a0ca7ecbe83542
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2025-08-10 03:32:00 -0600

    Enemy Animation Good

--- JetpackJoyride/Assets/Script/Obstacles/EnemyObstacle.cs
+++ JetpackJoyride/Assets/Script/Obstacles/EnemyObstacle.cs
@@ -11,10 +11,12 @@ public class EnemyObstacle : BasePoolObject, IObstacle
     private float randomJumpMax = 1.0f;
     private float forceMinAmount = 100.0f;
     private Rigidbody2D rb;
-    private bool onFloor = false;
     private BoxCollider2D boxCollider2D;
     private ParticleSystem deathParticleSystem;
     private SpriteRenderer spriteRenderer;
+
+    [Header("Animation")]
+    private Animator animator;
 
     protected override string ProvidePoolTag()
     {
@@ -34,6 +36,7 @@ public class EnemyObstacle : BasePoolObject, IObstacle
         boxCollider2D = GetComponent<BoxCollider2D>();
         deathParticleSystem = GetComponent<ParticleSystem>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         RandomJumpValue();
     }
 
@@ -47,6 +50,24 @@ public class EnemyObstacle : BasePoolObject, IObstacle
     {
         ObstacleAction();
         EnemyJump();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!GetIsFalling() && rb.velocity.y < 0.01f)
+        {
+            if (!GetOnFloor())
+            {
+                SetIsFalling(true);
+            }
+        }
+        else
+        {
+            if (!GetOnFloor())
+            {
+                SetIsJumping(true);
+            }
+        }
     }
 
     public void ObstacleAction()
@@ -63,7 +84,21 @@ public class EnemyObstacle : BasePoolObject, IObstacle
     {
         if (collision.gameObject.tag == "Floor")
         {
-            onFloor = true;
+            Debug.Log("On FLOOR");
+            SetOnFloor(true);
+            SetIsFalling(false);
+            SetIsJumping(false);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            if (!GetOnFloor())
+            {
+                SetOnFloor(true);
+            }
         }
     }
 
@@ -71,7 +106,7 @@ public class EnemyObstacle : BasePoolObject, IObstacle
     {
         if (collision.gameObject.tag == "Floor")
         {
-            onFloor = false;
+            SetOnFloor(false);
         }
     }
 
@@ -79,12 +114,13 @@ public class EnemyObstacle : BasePoolObject, IObstacle
     {
         currentJumpTime += Time.deltaTime;
 
-        if (onFloor)
+        if (GetOnFloor())
         {
             if (currentJumpTime >= maxJumpTime)
             {
                 if (rb != null)
                 {
+                    SetIsJumping(true);
                     rb.AddForce(Vector2.up * Random.Range(forceMinAmount, forceMaxAmount), ForceMode2D.Force);
                     RandomJumpValue();
                 }
@@ -127,6 +163,36 @@ public class EnemyObstacle : BasePoolObject, IObstacle
         yield return StartCoroutine(PlayDeathParticle());
 
         gameObject.SetActive(false);
+    }
+
+    private void SetIsFalling(bool _isFalling)
+    {
+        animator.SetBool("IsFalling", _isFalling);
+    }
+
+    private bool GetIsFalling()
+    {
+        return animator.GetBool("IsFalling");
+    }
+
+    private void SetIsJumping(bool _isJumping)
+    {
+        animator.SetBool("IsJumping", _isJumping);
+    }
+
+    private bool GetIsJumping()
+    {
+        return animator.GetBool("IsJumping");
+    }
+
+    private void SetOnFloor(bool _onFloor)
+    {
+        animator.SetBool("OnFloor", _onFloor);
+    }
+
+    private bool GetOnFloor()
+    {
+        return animator.GetBool("OnFloor");
     }
 
 
